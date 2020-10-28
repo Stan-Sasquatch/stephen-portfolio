1dbd802a4758fb10454c032178e90edd72f94b34
Stan-Sasquatch <stephenppercival@gmail.com>
2020-10-28 21:02:58 +0000

    Visuals on homepage

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -4,25 +4,26 @@ import '../CSS/HomePage.css';
 import PenguinPage from './PenguinPage';
 const HomePage = () => {
 
-    const escapedText = `<- that's me in the middle`
+
 
     return (<div><div className="flex-container-1"><h1>Home</h1>
         <h2>Website designed using React Version:{React.version}</h2></div>
         <div className="flex-container-2">
             <img className="profile-pic" src={Profile_Pic} alt="Stephen and two friends" />
-            <div className="escapedText">
-                Hi, I'm Stephen.<br />{escapedText}</div>
+
             <div className="homePageParagraph">
 
 
-                Welcome to my website!
+                Hi, I'm Stephen. Welcome to my website! (that's me in the middle)
                 <br />
                 Please browse the projects tab to have a look at what i've been coding recently.
                 <br />
                 Have a look at the about tab for more info about me!
 
         </div>
+
             <PenguinPage />
+
         </div></div>);
 }
 

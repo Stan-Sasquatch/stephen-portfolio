5d1c077a4d83a09078fd9981a54e9178857dc60c
Stan-Sasquatch <stephenppercival@gmail.com>
2021-02-07 16:01:38 +0000

    More styling homepage and about page

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -1,26 +1,40 @@
 import React from "react";
 import '../CSS/AboutPage.css';
 import About_Page_Pic from '../Utils/About_Page_Pic.jpg'
+import Climbing_Video from '../Utils/Climbing_Video.mp4'
 
 
 
 const AboutPage = () => {
-    return (<div><div className="container-1 text-center container-fluid col  row">
+    return (<div><div className="text-center container-fluid row">
         <h1 >About Stephen:</h1>
-        <div className="container-2  row">
+        <div className="container-2 row">
 
-            <img className="about-page-pic" src={About_Page_Pic} />
+            <div className="container-1 col-5">
 
-            {/* <div className="row container-3"><img className=" img-responsive about-page-pic " src={About_Page_Pic} alt="Stephen in Thailand" /></div> */}
-        </div></div><div className="container-3 col">
-            <p>
-                I'm 28 years old, originally from Wokingham, with a degree in Mathematics from the University of Sheffield.
-                <br />In my spare time I tend to be running, climbing, coding or playing in my band!
-    <br />
+                <img className="about-page-pic" src={About_Page_Pic} />
+            </div>
+            <div className="col-7 p-text">
+                <p>I'm 28 years old, originally from Wokingham, with a degree in Mathematics from the University of Sheffield.
+                <br />
                 My background has led me towards the financial sector, where I currently work, but I have always gravitated to the technical/system side of the companies i've worked in.
-                 Coding has been a hobby for me up until recently but I now feel i'm ready to transistion into a career in web development.
+                Coding has been a hobby for me up until recently but I now feel i'm ready to transistion into a career in web development.</p>
+
+            </div>
+        </div></div><div className="p-text row">
+            <div className="col-5">
+                <p>
+                In my spare time I tend to be running, climbing, coding or playing in my band!
+                   
 </p>
-        </div></div>);
+            </div>
+            <div className="col-7">
+                <video width="960" height="540" controls>
+                    <source src={Climbing_Video} type="video/mp4" />
+
+                    Your browser does not support the video tag.
+</video>
+            </div>  </div></div>);
 }
 
 export default AboutPage;
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -10,10 +10,35 @@ const HomePage = () => {
             <h2 >Stephen's Portfolio </h2>
         </div>
         <div className="row">
+            <div className="col-3"></div>
+            <div className="list-container text-center col-6">
 
+                <ul>
+                    <div >
+                        <li >
+                            Background in Mathematics, Finance and Data Handling
+</li>
+                    </div>
+                    <div>
+                        <li>
+                            Proficient in HTML, CSS, JavaScript and React
+</li>
+                    </div>
+                    <div>
+                        <li>
+                            Passionate about self-improvement
+</li>
+                    </div>
+                    <div>
+                        <li>
+                            Motivated to put the work in to learn new technologies
+</li>
+                    </div>
+                </ul>
+            </div>
 
-            <div className="text-center container-fluid  right-column">
-                <div className="row penguin">
+            <div className="text-center container-fluid  right-column col-3">
+                <div className="penguin">
                     <PenguinPage />
                 </div>
 
@@ -30,31 +55,7 @@ const HomePage = () => {
 
 
         </div>
-        <div className="list-container row">
 
-            <ul>
-                <div >
-                    <li >
-                        Background in Mathematics, Finance and Data Handling
-</li>
-                </div>
-                <div>
-                    <li>
-                        Proficient in HTML, CSS, JavaScript and React
-</li>
-                </div>
-                <div>
-                    <li>
-                        Passionate about self-improvement
-</li>
-                </div>
-                <div>
-                    <li>
-                        Motivated to put the work in to learn new technologies
-</li>
-                </div>
-            </ul>
-        </div>
 
 
 
